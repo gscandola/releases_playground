ce652a6becd9318adb7e40361a44f8279903e7cf
Guillaume Scandola <gscandola@meilleursagents.com>
2024-01-16 15:34:01 +0100

    ci: remove tag also

--- .circleci/scripts/createFinalRelease.mjs
+++ .circleci/scripts/createFinalRelease.mjs
@@ -9,7 +9,7 @@ const octokit = new Octokit({
 
 const repoInfos = { repo, owner };
 
-console.log('Fetching all releases of the repository...')
+console.log("Fetching all releases of the repository...");
 const data = await octokit.paginate(
   octokit.rest.repos.listReleases.endpoint.merge({
     ...repoInfos,
@@ -21,7 +21,9 @@ const drafts = data.filter((release) => release.draft);
 // Abort final release if draft releases found
 if (drafts.length) {
   console.log(
-    `Found ${drafts.length} draft release(s): ${drafts.map((p) => p.tag_name).join(",")}`
+    `Found ${drafts.length} draft release(s): ${drafts
+      .map((p) => p.tag_name)
+      .join(",")}`
   );
   console.log(
     "[ABORTING] Final release process ends here. Restart it by doing a release from the draft."
@@ -31,19 +33,29 @@ if (drafts.length) {
 
 const preReleases = data.filter((release) => release.prerelease);
 
-console.log(`${preReleases.length} pre-release(s) found, removing them...`)
-preReleases.forEach(async (preRelease) => {
-  await octokit.rest.repos.deleteRelease({
-    ...repoInfos,
-    release_id: preRelease.id,
-  });
-  console.log(`${preRelease.tag_name} deleted`);
-});
-console.log('Pre-release(s) deleted.')
+console.log(`${preReleases.length} pre-release(s) found, removing them...`);
+(async function () {
+  await Promise.all(
+    preReleases.forEach(async (preRelease) => {
+      await octokit.rest.repos.deleteRelease({
+        ...repoInfos,
+        release_id: preRelease.id,
+      });
+      await octokit.rest.git.deleteRef({
+        ...repoInfos,
+        ref: `tags/${preRelease.tag_name}`,
+      });
+      console.log(`${preRelease.tag_name} release & tag deleted`);
+    })
+  );
+})();
+console.log("Pre-release(s) deleted.");
 
-console.log('Dispatching repository event to trigger final release...')
+preReleases.forEach(async (preRelease) => {});
+
+console.log("Dispatching repository event to trigger final release...");
 await octokit.rest.repos.createDispatchEvent({
-    ...repoInfos,
-    event_type: 'pre_release_approved'
-})
-console.log('Event dispatched.')
+  ...repoInfos,
+  event_type: "pre_release_approved",
+});
+console.log("Event dispatched.");
